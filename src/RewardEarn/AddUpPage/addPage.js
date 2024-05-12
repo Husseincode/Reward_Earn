3a466b1337d715da7d6ab02e36b65b8f7a484a06
Aminat <aminatabiodun57@gmail.com>
2024-05-12 09:19:45 -1200

    new commits

--- src/RewardEarn/AddUpPage/addPage.js
+++ src/RewardEarn/AddUpPage/addPage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './addPage.css';
 import Logo from "../RewardHeader/Logo";
+import img from '../Assets/CADD4443-5504-4102-992A-858EB329F62A-removebg-preview.png'
 
 const AddPage = ({name}) => {
     return (
         <>
         <div className="addPage"></div>
         <div className="text text-white text-center">
-            <Logo/><hr/>
+            <img src={img} alt="King's reward"/>
             <button title="sign up" className="btn btn-light">Sign Up</button>
         </div>
         </>
--- src/RewardEarn/LoadingPage/LoadingPage.js
+++ src/RewardEarn/LoadingPage/LoadingPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import './LoadingPage.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-import img from '../Assets/k.png'
+import img from '../Assets/Kings .png'
 
 /**
  * @LoadingPage - This function returns the loading screen of a web page
@@ -10,20 +10,20 @@ import img from '../Assets/k.png'
  * @returns: Loading screen 
  */
 const LoadingPage = () => {
-    const [width, setWidth] = useState('50px');
+    const [width, setWidth] = useState('100px');
     useEffect(()=>{
         setTimeout(()=>{
-            if (width === '50px'){
-                setWidth('70px');
+            if (width === '100px'){
+                setWidth('150px');
             }
             else{
-                setWidth('50px');
+                setWidth('100px');
             }
         }, 1000)
     });
     return (
         <div className='loading'>
-            <img style={{transition: '1s'}} width={width} src={img}/>
+            <img style={{transition: '1s'}} alt="Loading page" width={width} src={img}/>
             {/* <FontAwesomeIcon style={{fontSize: '50px'}} icon={faSpinner} spin/> */}
         </div>
     );
